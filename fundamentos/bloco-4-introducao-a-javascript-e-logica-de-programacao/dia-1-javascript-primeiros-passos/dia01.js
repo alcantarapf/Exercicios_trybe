fe2a98df6c6dc595360b0ab2571b619d0b5b8d09
Pedro Alcântara <pedrofilho190893@hotmail.com>
2022-04-07 17:35:34 -0300

    arrays-for

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/dia01.js
@@ -0,0 +1,23 @@
+ let number1 = 2;
+ let number2 = 5;
+
+  console.log(number1 + number2);
+
+  let number4 = 3;
+  let number3 = 6;
+  console.log(number3 - number4);
+
+    let number5 = 10;
+    let number6 = 1;
+  console.log(number5 * number6);
+
+  let number7 = 3;
+  let number8 = 8;
+  console.log(number8 / number7);
+
+  let number9 = 20;
+  let number10= 100;
+  console.log(number9 % number10);
+ 
+  let numberX = 14
+  let
